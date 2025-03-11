4ab06e129645ea758511dcda0b8c381ba60f5d52
Nikita Khokhlov <awosart@gmail.com>
2025-03-11 16:47:35 +0800

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Nikita Khokhlov on 11.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
